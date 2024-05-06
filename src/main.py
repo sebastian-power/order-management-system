f220406677be91ba6dbebfb313e55eb77d29c8c9
bananameme <bananameme8@gmail.com>
2024-05-06 14:18:56 +1000

    fixed: "Entering N on first order doesn't stop order" fixed: create_order_item try/except loop logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from ui.Order_mgt_UI import Order_mgt_UI
 if __name__ == "__main__":
-    o_ui=Order_mgt_UI()
+    choice = 1
+    while choice != "1" and choice != "2":
+        choice = input("Would you like to create:\n(1): A store order \n(2): A postal order?\n")
+    o_ui=Order_mgt_UI(choice)
 
     for order in o_ui.orders:
         print(str(order))
