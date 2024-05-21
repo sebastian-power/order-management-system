3ebe2a0c10c1eba507e82d0eac1caa69370f609f
bananameme <bananameme8@gmail.com>
2024-05-21 10:23:51 +1000

    changed the estimated delivery time

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 from ui.Order_mgt_UI import Order_mgt_UI
+from datetime import datetime
+import random
+
 if __name__ == "__main__":
-    choice = 1
+    now = datetime.now()
+    choice = 0
     while choice != "1" and choice != "2":
         choice = input("Would you like to create:\n(1): A store order \n(2): A postal order?\n")
     o_ui=Order_mgt_UI(choice)
 
     for order in o_ui.orders:
         print(str(order))
+    
+    if choice == "1":
+        print("Thank you for your purchase. Have a nice day.")
+    else:
+        purchase_time = now.time()
+        print(purchase_time)
+
 
          
 #Remove an item
