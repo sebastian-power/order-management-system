53c5175835ff3b8a8733f943aafa32c267331840
Sebastian <52031320+sebastian-power@users.noreply.github.com>
2024-05-24 10:12:47 +1000

    Os paths (#12)
    * Formatted all python files w/ black formatter
    
    * Made open use pathlib so compatible across operating systems

--- src/order_app_logic_pkg/Order.py
+++ src/order_app_logic_pkg/Order.py
@@ -1,60 +1,65 @@
+from datetime import date, datetime
+
 from .Customer import Customer
 from .OrderItem import OrderItem
-from datetime import date
-from datetime import datetime
+
+
 class Order:
     order_id_base = "Order_"
-    order_num =0#ideally should be from a file
+    order_num = 0  # ideally should be from a file
 
-    def __init__(self, customer:Customer):
-        Order.order_num +=1
-        self.order_id=str(Order.order_num)
+    def __init__(self, customer: Customer):
+        Order.order_num += 1
+        self.order_id = str(Order.order_num)
         self.customer = customer
-        self.order_date= datetime.now()
+        self.order_date = datetime.now()
         self.items = []  # Initialize an empty list to store order items
 
     @property
-    def order_id(self)->type[str]:        
+    def order_id(self) -> type[str]:
         return self._order_id
-    
+
     @order_id.setter
-    def order_id(self, order_num:type[int]):
-        self._order_id=Order.order_id_base+'_'+order_num
-    
+    def order_id(self, order_num: type[int]):
+        self._order_id = Order.order_id_base + "_" + order_num
+
     @property
-    def order_date(self)->date:
+    def order_date(self) -> date:
         return self._order_date
-    
+
     @order_date.setter
-    def order_date(self,todays_date:datetime):
-        self._order_date=date(todays_date.year,todays_date.month,todays_date.day)
-    
-    def add_item(self, item:OrderItem):
+    def order_date(self, todays_date: datetime):
+        self._order_date = date(todays_date.year, todays_date.month, todays_date.day)
+
+    def add_item(self, item: OrderItem):
         self.items.append(item)
 
-    def remove_item(self, item:OrderItem)->type[str]:
-        result=""
+    def remove_item(self, item: OrderItem) -> type[str]:
+        result = ""
         if item in self.items:
             self.items.remove(item)
-            result="Order item was successfully removed"
+            result = "Order item was successfully removed"
         else:
-           result=f"{item} is not in the order."
+            result = f"{item} is not in the order."
         return result
 
-    def calculate_total(self)->type[float]:
+    def calculate_total(self) -> type[float]:
         total_price = sum(item.price for item in self.items)
         return total_price
-    
-    def __str__(self)->type[str]:
-        star_line = "-"*100+"\n"
-        order_header_details="Order Header\n"+\
-            f"Order ID:{self.order_id:20}" +\
-                f"Order date:{self.order_date}\n"
-        customer_details=str(self.customer)
-        item_details="Order items are:\n"+'\n'.join(map(str, self.items))+'\n'
-        return (star_line+order_header_details+customer_details+item_details+star_line)
 
-
-
-
-
+    def __str__(self) -> type[str]:
+        star_line = "-" * 100 + "\n"
+        order_header_details = (
+            "Order Header\n"
+            + f"Order ID:{self.order_id:20}"
+            + f"Order date:{self.order_date}\n"
+        )
+        customer_details = str(self.customer)
+        item_details = "Order items are:\n" + "\n".join(map(str, self.items)) + "\n"
+        return (
+            star_line
+            + order_header_details
+            + customer_details
+            + item_details
+            + star_line
+        )
