4eda832f4e0b39745ab9cff7b574bbc991e35c2c
Sebastian <52031320+sebastian-power@users.noreply.github.com>
2024-05-24 10:27:13 +1000

    gui on top (#15)
    Co-authored-by: bananameme <bananameme8@gmail.com>

---
+++ src/order_app_logic_pkg/Admin.py
@@ -0,0 +1,13 @@
+from datetime import date
+from datetime import datetime
+
+from .Customer import Customer
+
+class Admin(Customer):
+
+    def __init__(self):
+        super.__init__()
+
+    def view_all_orders():
+        pass
+        #for each order: call print_order(order_type)
