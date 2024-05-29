2cef641ae57447f29b8fc6a9df9027639aae6870
Sebastian <52031320+sebastian-power@users.noreply.github.com>
2024-05-29 12:44:47 +1000

    Add orders db (#22)
    * Database add statements
    
    * Added customer and order db functionality
    
    * here. we. go
    
    * last

--- src/order_app_logic_pkg/Admin.py
+++ src/order_app_logic_pkg/Admin.py
@@ -1,9 +1,14 @@
 from .Customer import Customer
+from db_app_logic.Order_DB import Order_DB
 
 class Admin(Customer):
 
     def __init__(self):
         super.__init__()
+        self.dbmgt = Order_DB()
+
+    def add_product(self, name, price):
+        self.dbmgt.add_product_to_db(name, price)
 
     def view_all_orders():
         pass
