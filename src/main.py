cbc6bdc021d55af1a6ef80b843084da4ef059c36
bananameme <bananameme8@gmail.com>
2024-06-03 14:19:36 +1000

    small changes idk i did this last week i dont remember what i changed, good luck.

--- src/main.py
+++ src/main.py
@@ -5,14 +5,15 @@ from ui.Order_mgt_UI import Order_mgt_UI
 if __name__ == "__main__":
     now = datetime.now()
     choice = 0
-    while choice != "1" and choice != "2" and choice != "3" and choice != "4":
+    while int(choice) != 1 and int(choice) != 1 and int(choice) != 3 and int(choice) != 4:
         choice = input(
-            "Would you like to:\n(1): Create a store order \n(2): Create a postal order?\n(3): Sign in as an admin?\n(4): Sign in as a customer?\n"
+            "Would you like to:\n(1): Create a store order \n(2): Create a postal order?\n(3): Sign in as a customer?\n(4): Sign in as an admin?\n"
         )
     o_ui = Order_mgt_UI(choice)
 
-    for order in o_ui.orders:
-        print(str(order))
+    if choice == "1" or choice == "2":
+        for order in o_ui.orders:
+            print(str(order))
 
     if choice == "1":
         print("Thank you for your purchase. Have a nice day.")
